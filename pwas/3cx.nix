1ce10edc28cc9687db07c22faeb64966bc405670
tom <xolik@xolik.eu>
2025-12-01 05:58:16 +0100

    revert: url

--- pwas/3cx.nix
+++ pwas/3cx.nix
@@ -1,13 +1,11 @@
 # pwas/3cx.nix
 # 3CX PWA desktop entry
 { pkgs, ... }:
-let
-  urls = import ../secrets/urls.nix;
-in
+
 {
   xdg.desktopEntries.tcx = {
     name = "3CX";
-    exec = "${pkgs.chromium}/bin/chromium --app=${urls.threecx}";
+    exec = "${pkgs.chromium}/bin/chromium --app=https://creativetech.il.3cx.us/webclient";
     icon = "phone";
     terminal = false;
     type = "Application";
