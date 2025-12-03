98bc2c29e1d3a843dae1065705be66c8bf653714
tom <xolik@xolik.eu>
2025-12-03 02:42:08 +0100

    change ghostty theme

--- modules/home-manager/programs/terminal.nix
+++ modules/home-manager/programs/terminal.nix
@@ -1,7 +1,17 @@
 # modules/home-manager/programs/terminal.nix
 # Terminal emulator configuration
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  programs.ghostty.enable = true;
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      theme = "TokyoNight";
+    };
+  };
 }
